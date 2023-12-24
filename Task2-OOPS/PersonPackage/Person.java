950f8a14558ba75254db8f475ba2653818e91e75
GokulGuvi123 <154435622+GokulGuvi123@users.noreply.github.com>
2023-12-24 19:19:23 +0530

    Update Person.java

--- Task2-OOPS/PersonPackage/Person.java
+++ Task2-OOPS/PersonPackage/Person.java
@@ -1,1 +1,23 @@
+package PersonPackage;
 
+public class Person {
+	public String name;
+	public int age;
+
+	// Constructor to initialize name and age
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public Person(String name) {
+		this.name = name;
+		this.age = 18;
+	}
+
+	// Method to display name and age of the person
+	public void displayInfo() {
+		System.out.println("Name :" + this.name + ", Age : " + this.age);
+	}
+
+}
