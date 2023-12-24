e644e7484feb094d9deda05e4d2b08ba2e246faa
GokulGuvi123 <154435622+GokulGuvi123@users.noreply.github.com>
2023-12-24 19:23:38 +0530

    Create Person.java

---
+++ Task2-OOPS/Person_Employee_Inheritance/Person.java
@@ -0,0 +1,12 @@
+package Person_Employee_Inheritance;
+
+public class Person {
+	protected String name;
+	protected int age;
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+}
