d11e2faab11c99549b368fba320e23eca2ef1eec
GokulGuvi123 <154435622+GokulGuvi123@users.noreply.github.com>
2023-12-24 19:20:12 +0530

    Update PersonProperties.java

--- Task2-OOPS/PersonPackage/PersonProperties.java
+++ Task2-OOPS/PersonPackage/PersonProperties.java
@@ -1,1 +1,15 @@
+package PersonPackage;
 
+public class PersonProperties {
+	public static void main(String[] args) {
+		Person p1 = new Person("Virat", 30);
+		p1.displayInfo();
+		Person p2 = new Person("Raghul", 20);
+		p2.displayInfo();
+		
+		//Crating a Person object with default age
+		Person p3 = new Person("Hardik");
+		p3.displayInfo();
+	}
+
+}
