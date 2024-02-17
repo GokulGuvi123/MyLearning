641da1db8f43e40fd921e774e242b7cec550cb0f
GokulGuvi123 <154435622+GokulGuvi123@users.noreply.github.com>
2024-02-17 20:56:07 +0530

    Create Weekdays.java

---
+++ Task4-ExceptionANDCollection/Weekdays_OutOfBoundException/Weekdays.java
@@ -0,0 +1,20 @@
+package Exception_Handling_OutofBound;
+
+import java.util.Scanner;
+
+public class Weekdays {
+	public static void main(String[] args) {
+		String[] weekDays = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter day position(0-6)");
+		int dayPos = scanner.nextInt();
+
+		try {
+			String dayName = weekDays[dayPos];
+			System.out.println("The day at position " + dayPos + " is: " + dayName);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Error: Day index is outside the range (0-6)");
+		}
+		scanner.close();
+	}
+}
