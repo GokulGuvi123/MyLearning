3eb977866bbc3484006cecd0dcc445d422ae4273
GokulGuvi123 <154435622+GokulGuvi123@users.noreply.github.com>
2024-02-17 20:38:13 +0530

    Create Voter.java

---
+++ Task4-ExceptionANDCollection/VoterManagement/Voter.java
@@ -0,0 +1,32 @@
+package Exception_Handling_Voter;
+
+public class Voter {
+	String voterID;
+	int age;
+	String name;
+
+	public Voter(String voterID, int age, String name) throws InvalidAgeException {
+		try {
+			if (age < 18) {
+				throw new InvalidAgeException("Invalid age for voter");
+			}
+			this.age = age;
+			this.name = name;
+			this.voterID = voterID;
+		} catch (InvalidAgeException e) {
+			 System.out.println("Error creating voter: " + e.getMessage());
+		}
+	}
+
+	public String getVoterID() {
+		return voterID;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
